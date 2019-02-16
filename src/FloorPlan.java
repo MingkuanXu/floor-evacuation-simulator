4fc121138015acb31601ae2abee53ba236a15b8d
Mingkuan Xu <mingkuan.xu@duke.edu>
2019-02-16 03:17:44 -0500

    Change matrix to int[][] type. Add a simple path search function.

--- src/FloorPlan.java
+++ src/FloorPlan.java
@@ -3,33 +3,73 @@ import java.util.List;
 
 public class FloorPlan {
 
-	private List<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>> ();
+	private int[][] matrix;
+	private int length;
+	private int width;
 	
 	public FloorPlan() {
 	}
 	
-	public FloorPlan(List<ArrayList<Integer>> m) {
+	public FloorPlan(int[][] m) {
 		this.matrix = m;
 	}
 	
-	public List<ArrayList<Integer>> getMatrix() {
+	public int[][] getMatrix() {
 		return matrix;
+	}
+	
+	/***
+	 * Set the coordinate (x,y) to a specific value. Should be used for debug only.
+	 * @param x
+	 * @param y
+	 * @param value
+	 */
+	public void setCoordinate(int x, int y, int value) {
+		matrix[x][y] = value;
+		return;
 	}
 	
 
 	/***
-	 * This method is used to find the shortest path to the nearest exit.
+	 * This method uses dfs to find the shortest path to the nearest exit.
 	 * @param x: starting coordinate x;
 	 * @param y: starting coordinate y;
 	 * @return a list of coordinates on the shortest path.
+	 * @author Hongbo Sun
 	 */
-	public List<ArrayList<Integer>> calculatePath(int x, int y){
+	public int[][] dfsSearchPath(int x, int y){
+		
+		if(matrix[x][y]!=0) return new int[0][0]; // If the starting point is not empty, return.
+		
 		// Need to be implemented by SHB.
 		
-		// Handling exception: if matrix[x][y] != 0 throw exception.
+		return new int[0][0];
+	}
+	
+	
+	/***
+	 * This method finds a random path without considering any barriers.
+	 * Should be used for testing UI only. Will be deleted later.
+	 * @author Mingkuan Xu
+	 */
+	public int[][] simplySearchPath(int x, int y) {
+		
+		if(matrix[x][y]!=0) return new int[0][0]; // If the starting point is not empty, return.
+		
+		int length = matrix.length; // Refer to x coordinate
+		int width = matrix[0].length; // Refer to y coordinate
 		
 		
-		return new ArrayList<ArrayList<Integer>>();
+		List<int[]> coordinates = new ArrayList<int[]>();
+		for(int i=x; i<length; i++) { 
+				coordinates.add(new int[] {i,y});
+		}
+		for(int j=y; j<width; j++) { 
+			coordinates.add(new int[] {length-1,j});
+		}
+
+		return coordinates.toArray(new int[0][0]);
+		
 	}
 	
 	/***
--- src/Starter.java
+++ src/Starter.java
@@ -1,15 +1,33 @@
 public class Starter {
 
 	public static void main(String[] args) {
-		String filename = "test.jpeg";
-		FloorPlan fp = LoadFromPicture.loadFromPicture(filename);
+		
+//		String filename = "test.jpeg";
+//		FloorPlan fp = LoadFromPicture.loadFromPicture(filename);
 	
 		//Test case
 		int x = 0;
 		int y = 0;
+		int[][] m = {
+				{0,1,1,1,1,1,1},
+				{1,0,0,0,0,0,0},
+				{1,1,0,0,0,0,0},
+				{1,1,1,1,0,1,1}
+				};
+		FloorPlan fp = new FloorPlan(m);
+		int[][] result = fp.simplySearchPath(x, y);
+		System.out.print("Test");
+		for(int[] line:result) {
+			for(int each:line) {
+				System.out.print(each);
+			}
+			System.out.println();
+		}
 		
 		fp.displayMatrix();
-		System.out.print(fp.calculatePath(x, y));
+		//System.out.print(fp.dfsSearchPath(x, y));
+		
+		
 
 	} 
 }
