110ee464d4d890dd3fe0d36f1dae674f2a6f2788
Mingkuan Xu <mingkuan.xu@duke.edu>
2019-02-17 01:05:54 -0500

    Correct Coordinate.jaa

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -6,8 +6,6 @@ public class Coordinate {
 		this.x = a;
 		this.y = b;
 	}
-<<<<<<< HEAD
-=======
 	
 	public boolean equals(Coordinate a) {
 		return (a.x==this.x && a.y==this.y);
@@ -17,5 +15,4 @@ public class Coordinate {
 	public String toString() {
 		return("(" + this.x + "," + this.y + ")");
 	}
->>>>>>> d5f292fb431c418855fd0a39fa7261e6f1a757a4
 }
